942cff6a88f4a53c0dce74f481e59091d47db4be
BairBuyrchiyev <buyrchib@tcd.ie>
2020-11-10 22:39:22 +0000

    fixing merge

--- LCA.java
+++ LCA.java
@@ -81,42 +81,4 @@ public class LCA
 
         return false;
     }
-
-    public static void main(String[] args) 
-    { 
-        LCA tree = new LCA();
-        tree.root = new Node(1);
-        tree.root.addChild((new Node(2)));
-        tree.root.getChild(1).addChild(new Node(3));
-        tree.root.getChild(1).getChild(1).addChild(new Node(4));
-        tree.root.getChild(1).getChild(1).getChild(1).addChild(new Node(7));
-        tree.root.getChild(1).addChild(new Node(5));
-        tree.root.getChild(1).getChild(2).addChild(new Node(6));
-        tree.root.getChild(1).getChild(2).getChild(1).addChild(new Node(7));
-        
-        System.out.println(tree.root.data);
-        System.out.println(tree.root.getChild(1).data);
-        System.out.println(tree.root.getChild(1).getChild(1).data + "   " + tree.root.getChild(1).getChild(2).data);
-        System.out.println(tree.root.getChild(1).getChild(1).getChild(1).data + "   " + tree.root.getChild(1).getChild(2).getChild(1).data);
-        System.out.println(tree.root.getChild(1).getChild(1).getChild(1).getChild(1).data + " " + tree.root.getChild(1).getChild(2).getChild(1).getChild(1).data);
-        
-        /*
-        
-        1
-        |
-        2
-        | \
-        3  5
-        |  |
-        4  6
-         \/
-         7
-         
-        */
-        
-        System.out.println("LCA(4, 5): " + tree.findLCA(4,5));
-        System.out.println("LCA(4, 6): " + tree.findLCA(4,6));
-        System.out.println("LCA(3, 4): " + tree.findLCA(3,4));
-        System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
-    } 
 } 
